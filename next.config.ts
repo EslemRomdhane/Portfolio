9bae03791b5916bd834fe327e0a890f4510023ef
eslemromdhane <eslemromdhane0@gmail.com>
2026-01-21 03:41:09 +0100

    config1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  experimental: {
-    turbo: false, 
-  },
 };
 
 export default nextConfig;
